a2d898a5aacb8b3014b7e3cc800a9a130ebcaba8
zahrafeisal <zahrafeisal3@gmail.com>
2024-07-02 10:13:35 +0300

    adds functions assigned to variables

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+
+  const fareTripler = createFareMultiplier(3);
+
+  const selectDifferentDrivers = function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+      return returnFirstTwoDrivers(drivers);
+  }
+
